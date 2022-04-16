de1ebebfd06703df108398e7021c1c0d07af2196
pujol99 <alexpujolcomet8@gmail.com>
2022-04-16 23:11:49 +0200

    scene ui part 1

---
+++ src/components/game/SceneUI.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="footer">
+        <div class="subfooter">
+            <div class="money">
+                <h2>50$</h2>
+            </div>
+            <div class="totalMoney">
+                <h5>450$</h5> 
+            </div>
+        </div>
+        <div class="mainFooter">
+            <div>
+                <div class="perks">
+                    <p>Perks</p> 
+                </div>
+            </div>
+            <div class="throw">
+                <div class="throw-content" :class="{ 'throw-content-disabled': stage !== 'Throw' }">
+                    <h3>Throw</h3>
+                </div>
+            </div>
+            <div>
+                <div class="vowels">
+                    <div class="vowel">A</div>
+                    <div class="vowel">E</div>
+                    <div class="vowel">I</div>
+                    <div class="vowel">O</div>
+                    <div class="vowel">U</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+    data() {
+        return {
+        };
+    },
+    mounted() {
+    },
+    computed: {
+        ...mapGetters({
+            stage: "stages/getCurrentStage",
+        }),
+    },
+};
+</script>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <SceneVisuals />
+        <SceneUI />
         <Welcome v-if="currentStage === 'Welcome'"/>
         <Rules v-if="currentStage === 'Rules'"/>
         <Title v-if="currentStage === 'Title'" :title="getRoundTitle"/>
